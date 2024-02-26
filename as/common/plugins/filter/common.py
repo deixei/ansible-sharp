607cadd21892cab725f7e6f281e7d7bde06e9d3e
marcio.parente <marcio.parente@swisslife-am.com>
2024-02-26 08:58:09 +0000

    Folder structure

---
+++ as/common/plugins/filter/common.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2024 Marcio Parente
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.plugins.filter.core import to_json, quote, to_native, from_yaml
---
+++ as/common/plugins/module_utils/as_common.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2024 Marcio Parente
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+import os
